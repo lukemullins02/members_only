7debb14e54958f1f6aebc3020590c9edc6dfcfc7
= <lukejmullins@gmail.com>
2026-03-17 09:41:27 -0400

    Add password validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,45 @@
 const renderSignUp = (req, res) => res.render("sign-up-form");
 const db = require("../db/queries");
 const { genPassword, validPassword } = require("../lib/passwordUtils");
+const { body, validationResult, matchedData } = require("express-validator");
 
-const createUser = async (req, res) => {
-  const { firstName, lastName, username, password } = req.body;
+const lengthErr = "must be 50 characters or less.";
+const passwordErr = "Passwords don't match.";
 
-  const { salt, hash } = genPassword(password);
+const validateUser = [
+  body("firstName")
+    .isLength({ max: 50 })
+    .withMessage(`First Name ${lengthErr}`),
+  body("lastName").isLength({ max: 50 }).withMessage(`Last Name ${lengthErr}`),
+  body("username"),
+  body("password"),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage(`${passwordErr}`),
+];
 
-  await db.insertUser(firstName, lastName, username, hash, salt);
+const createUser = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  res.redirect("/");
-};
+    if (!errors.isEmpty()) {
+      return res.status(400).render("sign-up-form", {
+        errors: errors.array(),
+      });
+    }
+
+    const { firstName, lastName, username, password } = matchedData(req);
+
+    const { salt, hash } = genPassword(password);
+
+    await db.insertUser(firstName, lastName, username, hash, salt);
+
+    res.redirect("/");
+  },
+];
 
 module.exports = {
   renderSignUp,
