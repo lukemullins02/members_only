a53a5ad85a4246c08c30fd20cfc37a320e2ee375
= <lukejmullins@gmail.com>
2026-03-18 15:44:50 -0400

    Add railway connection

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: "localhost",
-  user: `${process.env.USERNAME}`,
-  database: `${process.env.DB}`,
-  password: `${process.env.PASSWORD}`,
-  port: 5432,
+module.exports = new Pool({
+  connectionString: process.env.URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-module.exports = pool;
